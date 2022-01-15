0c690bd230f9679771530d344f0fbe1bd374db99
Даниил <den4ic2001@gmail.com>
2022-01-15 22:46:55 +0300

    updated styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,37 +3,42 @@
 @section('main')
     <main class="home-page">
         <div class="container">
-            <h2>Рекомендуемые новости</h2>
-            <section class="news-list">
-                @foreach ($news as $news_item)
-                    <div class="news-item">
-                        <div class="poser">
-                            <img class="image" src="/assets/images/notFound.png" alt="">
-                        </div>
-                        <div class="info">
-                            <h3 title="{{$news_item->description}}">{{$news_item->title}}</h3>
-                        </div>
-                        <div class="more-block">
-                            <a class="btn more-btn" href="{{route('news.show', ['item' => $news_item->id])}}">Подробнее</a>
+            <h2 class="mb-4">Рекомендуемые новости</h2>
+            <section class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                @foreach ($news as $news_item)<div class="col">
+                    <div class="card shadow-sm">
+                        <img src="/assets/images/notFound.png" class="bd-placeholder-img card-img-top" width="100%" height="252"  role="img">
+                        <div class="card-body">
+                            <h3 class="card-text">{{$news_item->title}}</h3>
+                            <p class="card-text">{{$news_item->description}}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a type="button" href="{{route('news.show', ['item' => $news_item->id])}}" class="btn btn-sm btn-outline-secondary">Читать</a>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Добавить в избранное</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                </div>
                 @endforeach
             </section>
-
-            <h2>Новости из вашего города</h2>
-            <section class="news-list">
-                @foreach ($cityNews as $news_item)
-                    <div class="news-item">
-                        <div class="poser">
-                            <img class="image" src="/assets/images/notFound.png" alt="">
-                        </div>
-                        <div class="info">
-                            <h3 title="{{$news_item->description}}">{{$news_item->title}}</h3>
-                        </div>
-                        <div class="more-block">
-                            <a class="btn more-btn" href="{{route('news.show', ['item' => $news_item->id])}}">Подробнее</a>
+            <h2 class="my-4">Новости из вашего города</h2>
+            <section class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mb-4">
+                @foreach ($cityNews as $news_item)<div class="col">
+                    <div class="card shadow-sm">
+                        <img src="/assets/images/notFound.png" class="bd-placeholder-img card-img-top" width="100%" height="252"  role="img">
+                        <div class="card-body">
+                            <h3 class="card-text">{{$news_item->title}}</h3>
+                            <p class="card-text">{{$news_item->description}}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a type="button" href="{{route('news.show', ['item' => $news_item->id])}}" class="btn btn-sm btn-outline-secondary">Читать</a>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Добавить в избранное</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                </div>
                 @endforeach
             </section>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}}</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,7 +17,7 @@
 <div class="wrapper">
     <header>
         <div class="container">
-            <a href="/" class="logo">Новости</a>
+            <a href="/" class="logo">Test-news</a>
             <ul class="links close" id="dropdown_menu">
                 <li class="link"><a href="/">Главная</a></li>
                 <li class="link"><a href="/news">Новости</a></li>
